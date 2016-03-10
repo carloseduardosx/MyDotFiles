3d8045c0919ea593189a5696dcf5b23409c8f220
Carlos Eduardo <carloseduardosx2015@gmail.com>
2016-03-10 13:52:07 -0300

    Add some functions and aliases

--- functions/source.bash
+++ functions/source.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 export FUNCTIONS=$HOME/MyDotFiles/functions
 
 . $FUNCTIONS/.android
 . $FUNCTIONS/.git_functions
 . $FUNCTIONS/.greenmile
 . $FUNCTIONS/.util
+. $FUNCTIONS/.java_functions
