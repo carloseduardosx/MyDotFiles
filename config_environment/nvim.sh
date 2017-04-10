a60f55fd3566590c5aec128af45805bc894172f5
Carlos Eduardo <carloseduardosx2015@gmail.com>
2017-04-09 22:57:39 -0300

    Sun 9 of Apr of 2017 at 22:57:39

--- config_environment/nvim.sh
+++ config_environment/nvim.sh
@@ -1,1 +1,14 @@
 installer "nvim" "brew install neovim/neovim/neovim"
+
+# Create base directories
+mkdir -p ~/.config/nvim/bundle
+
+# Install dein and nvim configuration
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh \
+&& sh ./dein_installer.sh ~/.config/nvim/bundle \
+&& cd .. \
+&& ln -s $(pwd)/init.vim ~/.config/nvim/init.vim \
+&& ln -s $(pwd)/tmux.conf ~/.tmux.conf \
+&& rm config_environment/dein_installer.sh \
+&& nvim -c 'call dein#install() | exit'
+
