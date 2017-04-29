65a9935c0cd9359b87a90a42ebb21a7aa290a8f0
carloseduardosx <carloseduardosx2015@gmail.com>
2017-04-28 23:11:52 -0300

    Fri 28 of Apr of 2017 at 23:11:52

--- config_environment/git.sh
+++ config_environment/git.sh
@@ -1,1 +1,6 @@
 installer "git" "brew install git --with-default-names" true
+
+cd .. \
+&& ln -sf `pwd`/git_template ~/.git_template \
+&& git config --global init.templatedir '~/.git_template' \
+&& cd config_environment
