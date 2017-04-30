9dce38f42b83048904b68c7895de75f19f9265d1
carloseduardosx <carloseduardosx2015@gmail.com>
2017-04-30 17:01:50 -0300

    Sun 30 of Apr of 2017 at 17:01:50

--- config_environment/git.sh
+++ config_environment/git.sh
@@ -3,4 +3,10 @@ installer "git" "brew install git --with-default-names" true
 cd .. \
 && ln -sf `pwd`/git_template ~/.git_template \
 && git config --global init.templatedir '~/.git_template' \
-&& cd config_environment
+&& cd config_environment \
+&& npm install -g diff-so-fancy \
+&& git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX" \
+&& git config --global color.diff-highlight.oldNormal "red bold" \
+&& git config --global color.diff-highlight.oldHighlight "red bold 52" \
+&& git config --global color.diff-highlight.newNormal "green bold" \
+&& git config --global color.diff-highlight.newHighlight "green bold 22"
--- config_environment/nvm.sh
+++ config_environment/nvm.sh
@@ -1,1 +1,1 @@
-installer "nvm" "brew install nvm && mkdir $HOME/.nvm && export NVM_DIR="$HOME/.nvm" && source "/usr/local/opt/nvm/nvm.sh" && nvm install stable"
+installer "nvm" "brew tap homebrew/core && brew install node && brew install nvm && mkdir $HOME/.nvm && export NVM_DIR="$HOME/.nvm" && source "/usr/local/opt/nvm/nvm.sh" && nvm install stable"
