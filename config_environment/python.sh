280deadd5d98d3f2d0c171220c1e47e7082881ff
carloseduardosx <carloseduardosx2015@gmail.com>
2017-09-22 15:30:09 -0300

    Fix python installation

--- config_environment/python.sh
+++ config_environment/python.sh
@@ -1,1 +1,1 @@
-installer "python" "brew install python --with-default-names && brew install python3" true
+installer "python" "brew install python --with-default-names && echo ${password} | sudo easy_install pip && brew install python3" true
