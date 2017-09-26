8981b1ad1f83bd1d67565b1c135dc8ec37c9bf9f
carloseduardosx <carloseduardosx2015@gmail.com>
2017-09-26 11:29:16 -0300

    Tue 26 of Sep of 2017 at 11:29:16

--- functions/source.bash
+++ functions/source.bash
@@ -5,10 +5,12 @@ export SECRETS=$HOME/Projects/MyDotFiles/secrets/functions
 
 . $FUNCTIONS/.android
 . $FUNCTIONS/.git_functions
-. $SECRETS/.greenmile
 . $FUNCTIONS/.util
 . $FUNCTIONS/.java_functions
 . $FUNCTIONS/.docker
 . $FUNCTIONS/.cordova
 . $FUNCTIONS/.babel
 . $FUNCTIONS/.corona
+. $SECRETS/.greenmile
+. $SECRETS/.agenda
+
