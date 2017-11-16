dbf4d2796b1073f755a5eec5c305c95c6a002c04
carloseduardosx <carloseduardosx2015@gmail.com>
2017-11-15 22:28:58 -0300

    Wed 15 of Nov of 2017 at 22:28:58

--- config_environment/oh_my_zsh.sh
+++ config_environment/oh_my_zsh.sh
@@ -2,5 +2,5 @@ if test -e "$HOME/oh-my-zsh.sh"; then
     info "oh-my-zsh already installed"
 else
     echo ${password} | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    cp .zshrc $HOME/.zshrc
+    ln -sfn `pwd`/.zshrc ~/.zshrc
 fi
