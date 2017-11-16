1a76b83de3ef7409570d1bab27ecc4c8589a87cb
carloseduardosx <carloseduardosx2015@gmail.com>
2017-11-15 22:04:39 -0300

    Wed 15 of Nov of 2017 at 22:04:39

--- config_environment/photoshop.sh
+++ config_environment/photoshop.sh
@@ -1,1 +1,1 @@
-installer "adobe-photoshop-cc" 'echo ${password} | brew cask install adobe-photoshop-cc'
+installer "adobe-photoshop-cs6" 'echo "${password}" | brew cask install adobe-photoshop-cs6'
